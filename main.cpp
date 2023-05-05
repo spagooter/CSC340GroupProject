15d81a6f3b7b85f1945c1cedb8a5dd3fa34f2619
dononvan velasquez <donovan.velasquez@yahoo.com>
2023-05-04 18:21:09 -0700

    removed iterator class

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-  linkedList<string> list;
-  list.add("mom");
-  list.add("dad");
-  list.add("uncle");
-  list.add("grandpa");
-  cout << list.begin()->getVal();
+	linkedList<string> list;
+	list.add("mom");
+	list.add("dad");
+	list.add("uncle");
+	list.add("grandpa");
+	list.print();
 
-  return 0;
+	return 0;
 }

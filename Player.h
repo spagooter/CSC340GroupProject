39f2951870d1635470377a9c4fa1feed6dc5b1a0
jimmie <jimmiewu25@gmail.com>
2023-05-07 21:29:59 -0700

    add dealer and stuff to player/main

--- Player.h
+++ Player.h
@@ -26,19 +26,18 @@ public:
         cardsValue = 0;
         numPlayers = 0;
     }
-
+    //************************** getters *********************************
     string getPlayerName(){return this->playerName;}
     int getNumCards(){return this->numCards;}
     int getCardsValue(){return this->cardsValue;}
     int getPlayerNum(){return this->numPlayers;}
     int getCashRemaining(){return this->cashRemaining;}
 
-    void setPlayerName(){
-        cout << "Enter player's name: " << endl;
+    //************************** setters *********************************
+    void setPlayerName(int i){
+        cout << "Enter Player" << i << "'s name: " << endl;
         string str;
         cin >> str;
-        cout << "str: " << str << endl;
-        //cout << "input name: " << str << endl;
         this->playerName = str;
     }
     void setPlayerName(string name){this->playerName = name;}
@@ -53,37 +52,68 @@ public:
         this->cashRemaining = bet;
     }
 
+    //****************************** add money (win) ***************************************
+    void addMoney(int num){
+        int addCash = this->getCashRemaining() + num;
+        this->setCashRemaining(addCash);
+    }
+
+    //****************************** subtract money (lose) *********************************
+    void subMoney(int num){
+        int subMoney = this->getCashRemaining() - num;
+        this->setCashRemaining(subMoney);
+    }
+
     //void hit();
     //void stand();
 
+    //****************************** get number of players****************************
     void startingPlayerNum(){
         int num;
-        cout << "Enter number of players: (max 5)" << endl;
+        cout << endl << "Enter number of players: (max 5)" << endl;
         cin >> num;
         setNumPlayers(num);
         cout << "Number of players: " << getPlayerNum() << endl;
+        addDealer();
         addPlayer();
     }
 
+    //***************************** add dealer to linked list**************************
+    void addDealer(){
+        Player* person = new Player;
+        person->setPlayerName("Dealer");
+        person->setCashRemaining(10000000);     //ten million dollars
+        cout <<  "\nDealer: Hello my name is Bob. Welcome to my table.\n\nWhat are your names? \n" << endl;
+        //cout << "Dealer: " << person->getPlayerName() << " cash remaining: " << person->getCashRemaining() << endl;
+        Players.addNode(*person);
+
+    }
+
+    //***************************** add player to linked list **************************
     void addPlayer(){
         Players = *new linkedList<Player>();
         Player* person = new Player;
+
         for(int i = 1; i <= numPlayers; i++){
-            cout << "for loop: " << i << endl;
-            //cout << "Enter player" << i << "'s name:" << endl;
-            //string name;
-            //getline(cin, name);
-            person->setPlayerName();
 
-            //cout << "Buy in amount: " << endl;
-            //int bet;
-            //cin >> bet;
-            person->setCashRemaining();
-
-            Players.addNode(*person);
+            char correct = false;
+            while(correct != true) {                    //allows user to correct name and buy in amount
+                person->setPlayerName(i);
+                person->setCashRemaining();
+                cout << "Player name: " << person->getPlayerName() << " Player bet: " << person->getCashRemaining() << endl;
+                cout << "Is this correct? Y/N" << endl;
+                cin >> correct;
+                if (correct == 'Y' || correct == 'y') {
+                    Players.addNode(*person);
+                    correct = true;
+                } else {
+                    cout << "Please enter the correct info." << endl;
+                }
+            }//end while
         }//end for loop
     }// end addPlayer
 
+    //******************************* print players *******************************
     void printPlayer(){
         Node<Player>* curr;
         curr = Players.begin();
@@ -92,7 +122,7 @@ public:
             cout << "Player bet: " << curr->getVal().getCashRemaining() << endl;
             curr = curr->getNextNode();
         }
-    }
+    }//end printPlayer()
 
 
 }; //end Player class
--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include "BlackJack.h"
 #include "Player.h"
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-  Deck *deckPtr = new Deck("standard");
-  deckPtr->print();
+ void startGame(){
+     cout << "Welcome to our BlackJack table." << endl;
+     Deck *deckPtr = new Deck("standard");
+     Player* player1 = new Player;
 
+     //deckPtr->print();
+     player1->startingPlayerNum();
+     player1->printPlayer();
+ }
+
+int main() {
+
+     startGame();
+  //Deck *deckPtr = new Deck("standard");
+  //deckPtr->print();
+
+
+  //Player* player1 = new Player;
+  //player1->startingPlayerNum();
+  //player1->printPlayer();
   return 0;
 }
